d0ff52da42ad894c306441fd45755f15fccb5342
devmaxm <max.maiokha.dev@gmail.com>
2025-05-14 10:19:33 +0300

    some fixes

--- backend/supabase/functions/api/middlewares/authRequired.ts
+++ backend/supabase/functions/api/middlewares/authRequired.ts
@@ -1,9 +1,11 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 export function authRequiredMiddleware(req, res, next) {
-    const token = req.headers.authorization?.split(' ')[1]
-  if (!token) return res.status(401).send('Unauthorized')
-    console.log(token)
+  const token = req.headers.authorization?.split(' ')[1]
+  if (!token) {
+    return res.status(401).send('Unauthorized')
+  }
+
   const supabase = createClient(Deno.env.get('SUPABASE_URL'), Deno.env.get('SUPABASE_ANON_KEY'), {
     global: {
       headers: {
@@ -12,22 +14,6 @@ export function authRequiredMiddleware(req, res, next) {
     },
   })
 
-  return supabase.auth.getUser()
-    .then(({ data: { user }, error }) => {
-      if (error || !user) {
-        console.log(error)
-        res.status(403).send('Forbidden')
-        return
-      }
-      // Attach the user to the request
-      req.user = user
-      next()
-    })
-    .catch(err => {
-      res.status(500).send('Internal Server Error')
-    })
-
-    
   return supabase.auth.getUser()
     .then(({ data: { user }, error }) => {
       if (error || !user) {
